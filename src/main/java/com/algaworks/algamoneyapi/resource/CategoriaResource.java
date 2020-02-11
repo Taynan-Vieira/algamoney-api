37e10594850336ee71c057da4666b8cfa09b619c
Taynan-Vieira <taynan.vieira.one@gmail.com>
2020-02-11 14:46:45 -0300

    3.5. Coleção vazia, o que retornar

--- src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.algaworks.algamoneyapi.resource;
 import com.algaworks.algamoneyapi.model.Categoria;
 import com.algaworks.algamoneyapi.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
