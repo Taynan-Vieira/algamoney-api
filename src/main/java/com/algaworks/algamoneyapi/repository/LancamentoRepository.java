0b473f03010ec622d351d1f5efcb7c5390e19c45
Taynan-Vieira <taynan.vieira.one@gmail.com>
2020-02-27 11:07:59 -0300

    5.9. Implementando a paginação de lançamentos

--- src/main/java/com/algaworks/algamoneyapi/repository/LancamentoRepository.java
+++ src/main/java/com/algaworks/algamoneyapi/repository/LancamentoRepository.java
@@ -1,8 +1,13 @@
 package com.algaworks.algamoneyapi.repository;
 
 import com.algaworks.algamoneyapi.model.Lancamento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
+
+	@Override
+	Page<Lancamento> findAll(Pageable pageable);
 
 }
