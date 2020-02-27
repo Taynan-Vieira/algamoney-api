eb6d6e083bd382d8f5234319eb92888f4e2c69b3
Taynan-Vieira <taynan.vieira.one@gmail.com>
2020-02-27 11:55:39 -0300

    6.1. Implementando autenticação Basic

--- src/main/java/com/algaworks/algamoneyapi/config/AlgamoneyAdapter.java
+++ src/main/java/com/algaworks/algamoneyapi/config/AlgamoneyAdapter.java
@@ -1,4 +1,4 @@
-package com.algaworks.algamoneyapi.adapter;
+package com.algaworks.algamoneyapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
